d42b9ca955ff033475bea8a3b853891f5b36df70
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-11-05 21:06:40 +0000

    finally commit

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from datetime import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -11,12 +11,12 @@ Base = declarative_base()
 class Users(Base):
     __tablename__ = 'Users'
     id = Column(Integer, primary_key=True)
-    Username = Column(String(250), nullable=False, unique=True)
-    Name = Column(String(250), nullable= False)
-    Lastname = Column(String(250), nullable= False)
-    BirthDay = Column(datetime(), default=datetime.now())
-    Pasword = Column(String(250), nullable= False)
-    Email = Column(String(250), nullable = False, unique= True)
+    username = Column(String(250), nullable=False, unique=True)
+    name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    birthday = Column(DateTime, default=datetime.now())
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
 
 class Planets(Base):
     __tablename__ = 'planet'
@@ -30,43 +30,43 @@ class Planets(Base):
     climate = Column(String(50), nullable=False)
     terrain = Column(String(50), nullable=False)
     surface_water = Column(Integer, nullable=False)
-    created = Column(datetime, nullable=False)
-    edited = Column(datetime, nullable=False)
+    created = Column(DateTime, nullable=False)
+    edited = Column(DateTime, nullable=False)
     url = Column(String(50), nullable=False)
-    
+
 class Characters(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    height = Column(Integer, primary_key=True)
-    mass = Column(Integer, primary_key=True)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
     skin_color = Column(String(50), nullable=False)
     hair_color = Column(String(50), nullable=False)
     eye_color = Column(String(50), nullable=False)
     birth_year = Column(String(50), nullable=False)
     gender = Column(String(50), nullable=False)
-    created = Column(datetime, nullable=False)
-    edited = Column(datetime, nullable=False)
+    created = Column(DateTime, nullable=False)
+    edited = Column(DateTime, nullable=False)
     homeworld = Column(String(250), nullable=False)
 
-    class FavoritePlanets(Base):
-      __tablename__ = 'fav_planet'
+class FavoritePlanets(Base):
+    __tablename__ = 'fav_planet'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('Users.id'))
     planet_id = Column(Integer, ForeignKey('planet.id'))
     user = relationship(Users)
-    Favorites = relationship(Planets)
+    favorites = relationship(Planets)
 
 class FavoriteCharacters(Base):
     __tablename__ = 'fav_character'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('Users.id'))
     character_id = Column(Integer, ForeignKey('character.id'))
     user = relationship(Users)
-    Favorites = relationship(Characters)
+    favorites = relationship(Characters)
 
-    def to_dict(self):
-        return {}
+def to_dict(self):
+    return {}
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+# Draw from SQLAlchemy base
+render_er(Base, "diagram.png")
